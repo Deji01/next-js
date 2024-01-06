69c1ac82c7cb3d9600128b99ec5d99ae91ea54cc
Ayodeji Yekeen <ayodejiyou@gmail.com>
2024-01-06 14:39:49 +0000

    get data

---
+++ pg-essays-ai/scripts/scrape.ts
@@ -0,0 +1,159 @@
+import * as cheerio from "cheerio";
+import axios from "axios";
+import { encode } from "gpt-3-encoder";
+import { PGChunk, PGEssay, PGJSON } from "@/types";
+
+const BASE_URL = "https://www.paulgraham.com";
+const CHUNK_SIZE = 200;
+
+const getLinks = async () => {
+  const html = await axios.get(`${BASE_URL}/articles.html`);
+  const $ = cheerio.load(html.data);
+  const tables = $("table");
+
+  const LinksArr: { url: string; title: string }[] = [];
+
+  tables.each((i, table) => {
+    if (i === 1) {
+      const links = $(table).find("a");
+      links.each((j, link) => {
+        const url = $(link).attr("href");
+        const title = $(link).text();
+
+        if (url && url.endsWith(".html") && title) {
+          const linkObj = {
+            url,
+            title,
+          };
+          LinksArr.push(linkObj);
+        }
+      });
+    }
+  });
+  return LinksArr;
+};
+
+const getEssays = async (url: string, title: string) => {
+  let essay: PGEssay = {
+    title: "",
+    url: "",
+    date: "",
+    content: "",
+    tokens: 0,
+    chunks: [],
+  };
+
+  const html = await axios.get(`${BASE_URL}/${url}`);
+  const $ = cheerio.load(html.data);
+
+  const tables = $("table");
+
+  tables.each((i, table) => {
+    if (i === 1) {
+      const text = $(table).text();
+      let cleanedText = text
+        .replace(/\s+/g, " ")
+        .replace(/\.([a-zA-Z])/g, ". $1");
+
+      const split = cleanedText.match(/([A-Z][a-z]+ [0-9]{4})/);
+      let dateStr = "";
+      let textWithoutDate = "";
+
+      if (split) {
+        dateStr = split[0];
+        textWithoutDate = cleanedText.replace(dateStr, "");
+      }
+
+      let essayText = textWithoutDate.replace(/\n/g, " ").trim();
+      essay = {
+        title,
+        url: `${BASE_URL}/${url}`,
+        date: dateStr,
+        content: essayText,
+        tokens: encode(essayText).length,
+        chunks: [],
+      };
+    }
+  });
+  return essay;
+};
+
+const getChunks = (essay: PGEssay) => {
+  const { title, url, date, content } = essay;
+  let essayTextChunks: string[] = [];
+
+  if (encode(content).length > CHUNK_SIZE) {
+    const split = content.split(". ");
+    let chunkText = "";
+
+    for (let i = 0; i < split.length; i++) {
+      const sentence = split[i];
+      const sentenceTokenLength = encode(sentence).length;
+      const ChunkTokenLength = encode(chunkText).length;
+
+      if (ChunkTokenLength + sentenceTokenLength > CHUNK_SIZE) {
+        essayTextChunks.push(chunkText);
+        chunkText = "";
+      }
+
+      if (sentence[sentence.length - 1].match(/[a-z0-9]/i)) {
+        chunkText += sentence + ". ";
+      } else {
+        chunkText += sentence + " ";
+      }
+    }
+    essayTextChunks.push(chunkText.trim());
+  } else {
+    essayTextChunks.push(content.trim());
+  }
+
+  const essayChunks = essayTextChunks.map((chunkText, i) => {
+    const chunk: PGChunk = {
+      essay_title: title,
+      essay_url: url,
+      essay_date: date,
+      content: chunkText,
+      content_tokens: encode(chunkText).length,
+      embedding: [],
+    };
+
+    return chunk;
+  });
+
+  if (essayChunks.length > 1) {
+    for (let i = 0; i < essayChunks.length; i++) {
+      const chunk = essayChunks[i];
+      const prevChunk = essayChunks[i - 1];
+
+      if (chunk.content_tokens < 100 && prevChunk) {
+        prevChunk.content += " " + chunk.content;
+        prevChunk.content_tokens = encode(prevChunk.content).length;
+        essayChunks.splice(i, 1);
+      }
+    }
+  }
+
+  const chunkedEssay: PGEssay = {
+    ...essay,
+    chunks: essayChunks,
+  };
+
+  return chunkedEssay;
+};
+
+(async () => {
+  const links = await getLinks();
+
+  let essays: PGEssay[] = [];
+
+  for (let i = 0; i < links.length; i++) {
+    const link = links[i];
+
+    if (i === 7) {
+      const essay = await getEssays(link.url, link.title);
+      const chunkedEssay = getChunks(essay);
+      essays.push(chunkedEssay);
+      console.log(essay);
+    }
+  }
+})();
